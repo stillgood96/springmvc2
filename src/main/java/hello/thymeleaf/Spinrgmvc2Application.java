8cee801174158848a98d5b171a1334d964516f77
stillgood96 <a787574@naver.com>
2023-12-13 00:03:00 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleaf/Spinrgmvc2Application.java
@@ -0,0 +1,14 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spinrgmvc2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Spinrgmvc2Application.class, args);
+
+  }
+
+}
