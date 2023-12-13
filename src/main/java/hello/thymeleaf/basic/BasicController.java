4bd31e7b6d83699e27feaf0bc48c70f863d97de5
stillgood96 <a787574@naver.com>
2023-12-13 21:46:39 +0900

    변수 - SpringEL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,6 +1,12 @@
 package hello.thymeleaf.basic;
 
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +31,39 @@ public class BasicController {
     model.addAttribute("data", "hello <b>Spring</b>");
 
     return "basic/text-unescaped";
+  }
+
+  @GetMapping("variable")
+  public String variable(Model model) {
+
+    User userA = new User("uesrA", 10);
+    User userB = new User("uesrB", 20);
+
+    List<User> userList = new ArrayList<>();
+    userList.add(userA);
+    userList.add(userB);
+
+    Map<String, User> map = new HashMap<>();
+    map.put("userA", userA);
+    map.put("userB", userB);
+
+    model.addAttribute("user", userA);
+    model.addAttribute("users", userList);
+    model.addAttribute("userMap", map);
+
+    return "basic/variable";
+  }
+
+  @Data
+  static class User {
+
+    private String username;
+    private int age;
+
+    public User(String username, int age) {
+      this.username = username;
+      this.age = age;
+    }
   }
 
 
