5bf9f1f45cbbac4592c89701a89e8dab9bcd1aa8
stillgood96 <a787574@naver.com>
2023-12-17 16:47:47 +0900

    기본 객체들

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,12 +1,16 @@
 package hello.thymeleaf.basic;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -52,6 +56,25 @@ public class BasicController {
     model.addAttribute("userMap", map);
 
     return "basic/variable";
+  }
+
+  @GetMapping("/basic-objects")
+  public String basicObjects(Model model, HttpServletRequest request,
+                             HttpServletResponse response,HttpSession session) {
+
+    session.setAttribute("sessionData", "Hello Session");
+    model.addAttribute("request", request);
+    model.addAttribute("response", response);
+    model.addAttribute("servletContext", request.getServletContext());
+
+    return "basic/basic-objects";
+  }
+
+  @Component("helloBean")
+  static class HelloBean {
+    public String hello(String data) {
+      return "Hello " + data;
+    }
   }
 
   @Data
