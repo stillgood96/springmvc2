1d826f64aecadc938025f23d11e73a25e6615a11
stillgood96 <a787574@naver.com>
2023-12-17 20:04:34 +0900

    자바스크립트 인라인

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -128,6 +128,13 @@ public class BasicController {
     return "basic/block";
   }
 
+  @GetMapping("/javascript")
+  public String javascript(Model model) {
+
+    model.addAttribute("user", new User("UserA", 10));
+    addUsers(model);
+    return "basic/javascript";
+  }
 
 
   @Component("helloBean")
