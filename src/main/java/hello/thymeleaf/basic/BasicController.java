3977f489f1176ed014e68df65f566c69b1eb8a08
stillgood96 <a787574@naver.com>
2023-12-17 18:04:33 +0900

    블록

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -122,14 +122,13 @@ public class BasicController {
     return "basic/comments";
   }
 
-  private void addUsers(Model model) {
-    List<User> list = new ArrayList<>();
-    list.add(new User("userA", 10));
-    list.add(new User("userB", 20));
-    list.add(new User("userC", 30));
-
-    model.addAttribute("users", list);
+  @GetMapping("/block")
+  public String block(Model model) {
+    addUsers(model);
+    return "basic/block";
   }
+
+
 
   @Component("helloBean")
   static class HelloBean {
@@ -148,6 +147,15 @@ public class BasicController {
       this.username = username;
       this.age = age;
     }
+  }
+
+  private void addUsers(Model model) {
+    List<User> list = new ArrayList<>();
+    list.add(new User("userA", 10));
+    list.add(new User("userB", 20));
+    list.add(new User("userC", 30));
+
+    model.addAttribute("users", list);
   }
 
 }
